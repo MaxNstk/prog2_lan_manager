0b3ad476b172198bda9085dec43ac6e55b1672dc
JEKrieger <joaoeduardokrieger123@gmail.com>
2022-06-04 15:04:14 -0300

    customers

--- prog2_lan_manager/src/main/java/controllers/App.java
+++ prog2_lan_manager/src/main/java/controllers/App.java
@@ -24,17 +24,21 @@ import views.MainView;
  * @author max
  */
 public class App {
-
+    
     public static void main(String[] args) {
         LoginView loginView = new LoginView();
         loginView.setVisible(true);
-/*
+        
         CustomerDAO customerDAO = new CustomerDAO();
-        Customer leandro = new Customer("Leandro", "11122233344", "Ibirama", birthDate);
-        Customer marcos = new Customer("Marcos", "22233344455", "", birthDate);
-        Customer maria = new Customer("Maria", "33344455566", adress, birthDate);
-        Customer ana = new Customer("Ana", "44455566677", adress, birthDate);
-*/
+        Customer leandro = new Customer("Leandro", "11122233344", "Rua Alvarez, 123", "09112001");
+        customerDAO.createCustomer(leandro);
+        Customer marcos = new Customer("Marcos", "22233344455", "Rua Stein, 233", "10022004");
+        customerDAO.createCustomer(marcos);
+        Customer maria = new Customer("Maria", "33344455566", "Avenida Ramos, 333", "17051999");
+        customerDAO.createCustomer(marcos);
+        Customer ana = new Customer("Ana", "44455566677", "Rua Gertrudes", "25072000");
+        customerDAO.createCustomer(marcos);
+        
         DeviceDAO deviceDAO = new DeviceDAO();
         Computer computador1 = new Computer("Computador 1", "Intel i5 | RAM DDR4 8GB | GTX1650 2GB", true);
         deviceDAO.createDevice(computador1);
@@ -50,7 +54,7 @@ public class App {
         deviceDAO.createDevice(xboxSeriesS);
         Console playstation4 = new Console("Playstation 4", true);
         deviceDAO.createDevice(xboxSeriesS);
-
+        
         CategoryDAO categoryDAO = new CategoryDAO();
         Category fps = new Category(40, "FPS");
         categoryDAO.createCategory(fps);
@@ -58,7 +62,7 @@ public class App {
         categoryDAO.createCategory(estrategia);
         Category battleRoyale = new Category(60, "Battle Royale");
         categoryDAO.createCategory(battleRoyale);
-
+        
         GameDAO gameDAO = new GameDAO();
         Game ageOfEmpires = new Game("Age of Empires", "Civilizações ancestrais", estrategia);
         gameDAO.createGame(ageOfEmpires);
@@ -69,54 +73,54 @@ public class App {
         Game Warzone = new Game("Warzone", "Armas e sobrevivência", battleRoyale);
         gameDAO.createGame(Warzone);
     }
-
+    
     public static void openMainScreen() {
         MainView mainView = new MainView();
         mainView.setVisible(true);
     }
-
+    
     public static void openGameCreateView() {
         GameFormView gameFormView = new GameFormView(null);
         gameFormView.setVisible(true);
     }
-
+    
     public static void openGameUpdateView(Game game) {
         GameFormView gameCreateView = new GameFormView(game);
         gameCreateView.setVisible(true);
     }
-
+    
     public static void openGameListView() {
         GameListView gameListView = new GameListView();
         gameListView.setVisible(true);
     }
-
+    
     public static void openCustomerCreateView() {
         CustomerFormView customerCreateView = new CustomerFormView(null);
         customerCreateView.setVisible(true);
     }
-
+    
     public static void openCustomerUpdateView(Customer customer) {
         CustomerFormView customerCreateView = new CustomerFormView(customer);
         customerCreateView.setVisible(true);
     }
-
+    
     public static void openCustomerListView() {
         CustomerListView customerListView = new CustomerListView();
         customerListView.setVisible(true);
     }
-
+    
     public static void openGamePlayFormView() {
         GameplayFormView gameplayFormView = new GameplayFormView();
         gameplayFormView.setVisible(true);
     }
-
+    
     public static void openAddCreditsView(int id) {
         AddCreditsView addCreditsView = new AddCreditsView(id);
         addCreditsView.setVisible(true);
     }
     
-    public static void openGameplayListView(){
+    public static void openGameplayListView() {
         GameplayListView gameplayListView = new GameplayListView();
         gameplayListView.setVisible(true);
-    }   
+    }    
 }
